b051ab9ac2d6c229ffab040b7086df2209654a30
Miguel Santiago <miguel.santiago@solarwinds.com>
2018-03-27 00:08:49 +0100

    Run nginx using docker

---
+++ 01-docker-nginx/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker run --name nginx \
+    -v $DIR/../html:/usr/share/nginx/html:ro \
+    -v $DIR/../gifs:/usr/share/nginx/images/gifs:ro \
+    -v $DIR/../conf/default.conf:/etc/nginx/conf.d/default.conf:ro \
+    -p 8080:80 \
+    --rm nginx
